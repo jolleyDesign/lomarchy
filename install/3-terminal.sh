e3565129d0a63fda1f1632a77e1dea9841876b38
Alex Jolley <itisjolley+github@gmail.com>
2025-07-02 07:54:25 -0600

    Update 3-terminal.sh

--- install/3-terminal.sh
+++ install/3-terminal.sh
@@ -1,6 +1,6 @@
 yay -S --noconfirm --needed \
   wget curl unzip inetutils \
-  eza fzf ripgrep zoxide bat \
-  wl-clipboard fastfetch btop \
+  fzf ripgrep zoxide bat \
+  wl-clipboard btop \
   man tldr less whois plocate \
-  alacritty
+  ghostty
